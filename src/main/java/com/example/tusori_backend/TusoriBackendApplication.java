f097dbb37c814ec16dc70a096b3e1352d1e23385
0lYUMA <102151689+0lYUMA@users.noreply.github.com>
2024-03-19 16:28:34 +0900

    inital commit

---
+++ src/main/java/com/example/tusori_backend/TusoriBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.tusori_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TusoriBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TusoriBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tusori_backend/TusoriBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tusori_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TusoriBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
